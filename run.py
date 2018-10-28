e781496f459d177e7f9ce1183d194418df9d8945
Pauline <paulinemaurylaribiere@gmail.com>
2018-10-28 19:12:43 +0100

    run filled

--- run.py
+++ run.py
@@ -22,16 +22,16 @@ y_train_2 = y_train[idx_2_tr]
 y_train_3 = y_train[idx_3_tr]
 
 # Pre-processing of each category
-cat_0_tr, cat_0_te = dataprocessing(cat_0_tr, cat_0_te, degree = 5, adddegree = True, inv = True, frac = False, sqroot = True, sqrootpos = True, cbroot = True, comb = True, comb3 = True, trigo=True, expo = False, hyperb=False,combtrigo=False)
-cat_1_tr, cat_1_te = dataprocessing(cat_1_tr, cat_1_te, degree = 5, adddegree = True, inv = True, frac = False, sqroot = True, sqrootpos = True, cbroot = True, comb = True, comb3 = True, trigo=True, expo = False, hyperb=False,combtrigo=False)
-cat_2_tr, cat_2_te = dataprocessing(cat_2_tr, cat_2_te, degree = 5, adddegree = True, inv = True, frac = False, sqroot = True, sqrootpos = True, cbroot = True, comb = True, comb3 = True, trigo=True, expo = False, hyperb=False,combtrigo=False)
-cat_3_tr, cat_3_te = dataprocessing(cat_3_tr, cat_3_te, degree = 5, adddegree = True, inv = True, frac = False, sqroot = True, sqrootpos = True, cbroot = True, comb = True, comb3 = True, trigo=True, expo = False, hyperb=False,combtrigo=False)
+cat_0_tr, cat_0_te = dataprocessing(cat_0_tr, cat_0_te, degree = 5, adddegree = True, inv = True, frac = False, sqroot = True, sqrootpos = True, cbroot = True, comb = True, comb3 = False, trigo=True, expo = False, hyperb=False,combtrigo=False)
+cat_1_tr, cat_1_te = dataprocessing(cat_1_tr, cat_1_te, degree = 5, adddegree = True, inv = True, frac = False, sqroot = True, sqrootpos = True, cbroot = True, comb = True, comb3 = False, trigo=True, expo = False, hyperb=False,combtrigo=False)
+cat_2_tr, cat_2_te = dataprocessing(cat_2_tr, cat_2_te, degree = 5, adddegree = True, inv = True, frac = False, sqroot = True, sqrootpos = True, cbroot = True, comb = True, comb3 = False, trigo=True, expo = False, hyperb=False,combtrigo=False)
+cat_3_tr, cat_3_te = dataprocessing(cat_3_tr, cat_3_te, degree = 5, adddegree = True, inv = True, frac = False, sqroot = True, sqrootpos = True, cbroot = True, comb = True, comb3 = False, trigo=True, expo = False, hyperb=False,combtrigo=False)
 
 # Only keep useful columns of each category
-feat_0 = []
-feat_1 = []
-feat_3 = []
-feat_4 = []
+feat_0 = [137, 94, 293, 104, 21, 18, 35,30,91,143,151,242,154,138,97,264,19,251,158,224,296,211]
+feat_1 = [114, 40,134,403,189,192,412,22,342,213,233,88,111,211,23,228,190,194,82,206,73,295,54]
+feat_3 = [313,142,258,637,153,284,423,227,49,165,145,29,231,42,156,30,226,170,58,485,384,413,35,91,171,309,169,254,47,96,198,162,478,45]
+feat_4 = [313, 338,142,258,145,234,187,503,644,618,284,147,240,411,31,29,555,119,634,408,402]
 
 cat_0_tr = cat_0_tr[:,feat_0]
 cat_1_tr = cat_1_tr[:,feat_1]
@@ -45,10 +45,10 @@ cat_3_te = cat_3_te[:,feat_3]
 
 
 # Define regularization parameter
-lamb_0 = 
-lamb_1 = 
-lamb_2 = 
-lamb_3 = 
+lamb_0 = 0.000032903 
+lamb_1 = 0.000108264 
+lamb_2 = 0.000010000 
+lamb_3 = 0.000108264 
 
 # Find weights of each category
 w_best_0, _ = ridge_regression(y_train_0, cat_0_tr, lamb_0)
